af57b733f9e20f3db97ceba36580d0a8fcd958f9
albertbilu32@gmail.com <albertbilu32@gmail.com>
2026-08-19 18:07:20 +0000

    Commit Changes...

--- OOP/classes_inheritance.py
+++ OOP/classes_inheritance.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ##### TBD
+
+# COMMAND ----------
+
 # DBTITLE 1,Cell 1
 class Level1:
     variable_1 = 100
@@ -39,6 +44,22 @@ print(obj.variable_1, obj.var_1, obj.fun_1())
 print(obj.variable_2, obj.var_2, obj.fun_2())
 print(obj.variable_3, obj.var_3, obj.fun_3())
     
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###### Inheritance is not the only way to construct adaptable classes. 
+# MAGIC #### You can achieve the same goals (not always, but very often) by using a technique named COMPOSITION.
+# MAGIC
+# MAGIC Composition is the process of composing an object using other different objects. The objects used in the composition deliver a set of desired traits (properties and/or methods) so we can say that they act like blocks used to build a more complicated structure.
+# MAGIC
+# MAGIC It can be said that:
+# MAGIC
+# MAGIC -     inheritance extends a class's capabilities by adding new components and modifying existing ones; in other words, the complete recipe is
+# MAGIC       contained inside the class itself and all its ancestors; the object takes all the class's belongings and makes use of them;
+# MAGIC -     composition projects a class as a container able to store and use other objects (derived from other classes) where each of the objects
+# MAGIC       implements a part of a desired class's behavior.
+# MAGIC
 
 # COMMAND ----------
 
